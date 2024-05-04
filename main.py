70707784c69734110cdcf777c96ad409eadd9863
Daniel Roland <its.daniel.roland+github@gmail.com>
2024-05-04 18:15:25 -0500

    refactor: Clean imports and reorganize code

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ interface.
 
 import sys
 import os
-import tensorflow as tf
 
 # Only allow Tensorflow to print errors.
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
 from parse import parse_args, parse_toml
-from dataset import build_dataset
+from model.builder import build_model, build_optimizer, build_callbacks
+from dataset import build_dataset, augment_dataset
 
 
 def main():
@@ -19,50 +19,9 @@ def main():
     Configures and tests a model on a dataset according to command-line
     arguments.
     """
-    cannot_continue = False
-
     # Looking for mandatory arguments.
     args = parse_args(sys.argv[1:])
-    if "dataset" not in args:
-        print("Missing required parameter `--dataset=<dataset>`")
-        cannot_continue = True
-    if "model" not in args:
-        print("Missing required parameter `--model=<model>`")
-        cannot_continue = True
-
-    if cannot_continue:
-        print("An irrecoverable error occurred and the program must abort.")
-        return
-
-    # Ensuring that configurations exist.
-    dataset_path = "configs/dataset-" + args["dataset"] + ".toml"
-    model_path = "configs/model-" + args["model"] + ".toml"
-    if not os.path.exists(dataset_path):
-        print(f"Unable to locate dataset configuration at [{dataset_path}].")
-        cannot_continue = True
-    if not os.path.exists(model_path):
-        print(f"Unable to locate model configuration at [{model_path}].")
-        cannot_continue = True
-
-    if cannot_continue:
-        print("An irrecoverable error occurred and the program must abort.")
-        return
-
-    # Loading configurations.
-    try:
-        dataset_config = parse_toml(dataset_path)
-    except:
-        print(f"The dataset configuration at [{dataset_path}] was malformed.")
-        cannot_continue = True
-    try:
-        model_config = parse_toml(model_path)
-    except:
-        print(f"The model configuration at [{model_path}] was malformed.")
-        cannot_continue = True
-
-    if cannot_continue:
-        print("An irrecoverable error occurred and the program must abort.")
-        return
+    dataset_config, model_config = get_configs_from(args)
 
     # Constructing the dataset.
     dataset_block = build_dataset(dataset_config["dataset"])
@@ -95,10 +54,48 @@ def main():
     )
 
     results = model.evaluate(
-        testing, batch_size=model_config["hyperparams"]["batch_size"],
+        testing,
+        batch_size=model_config["hyperparams"]["batch_size"],
     )
 
     print(results)
+
+
+def get_configs_from(args):
+    errors = []
+    if "dataset" not in args:
+        errors.append("Missing required parameter `--dataset=<dataset>`")
+    if "model" not in args:
+        errors.append("Missing required parameter `--model=<model>`")
+
+    if len(errors) > 0:
+        raise KeyError(*errors)
+
+    # Ensuring that configurations exist.
+    dataset_path = "configs/dataset-" + args["dataset"] + ".toml"
+    model_path = "configs/model-" + args["model"] + ".toml"
+    if not os.path.exists(dataset_path):
+        errors.append(f"Unable to locate dataset configuration at [{dataset_path}].")
+    if not os.path.exists(model_path):
+        errors.append(f"Unable to locate model configuration at [{model_path}].")
+
+    if len(errors) > 0:
+        raise KeyError(*errors)
+
+    # Loading configurations.
+    try:
+        dataset_config = parse_toml(dataset_path)
+    except Exception as e:
+        errors.append(e)
+    try:
+        model_config = parse_toml(model_path)
+    except Exception as e:
+        errors.append(e)
+
+    if len(errors) > 0:
+        raise ValueError(*errors)
+
+    return dataset_config, model_config
 
 
 if __name__ == "__main__":
